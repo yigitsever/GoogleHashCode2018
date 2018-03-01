c0aea4929b379f35a04cc94bb2a856823ddcc005
Megalodone <coolnavarin@gmail.com>
2018-03-01 22:24:05 +0300

    added constructor

--- src/Coord.java
+++ src/Coord.java
@@ -4,6 +4,11 @@ public class Coord {
     int x;
     int y;
 
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
     public int distTo(Coord o) {
         return Math.abs(this.x - o.x) + Math.abs(this.y - o.y);
     }
