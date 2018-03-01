bbff3dc70f43af7415472be5f154207149a2af45
Megalodone <coolnavarin@gmail.com>
2018-03-01 21:57:17 +0300

    filled City and Ride

--- src/City.java
+++ src/City.java
@@ -1,6 +1,15 @@
+import java.util.*;
+
 public class City {
 
     public static void main(String[] args) {
+
+        Queue<Ride> rides = new LinkedList<Ride>();
+        ArrayList <Car> Vehicle=new ArrayList<>();
         
-    }
+
+
+
+
+
 }
