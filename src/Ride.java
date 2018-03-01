b90ecca948096e0e9d870330c06b19fedb626568
cihadtekinbas <Matematik97@>
2018-03-01 21:19:15 +0200

    filled ride

--- src/Ride.java
+++ src/Ride.java
@@ -1,19 +1,18 @@
-public class Ride{
-    public int destination;
+public class Ride {
+
     public int Time_start;
+    public int Time_latest;
+    public int potential_score;
+    public Coord start;
+    public Coord dest;
 
-    public  void Ride(){
-
-
-
+    public void Ride(Coord start, Coord dest, int T_start, int T_latest) {
+        this.Time_start = T_start;
+        this.Time_latest = T_latest;
+        this.start=start;
+        this.dest=dest;
 
     }
-    public void find_dest(Coord start ,Coord finish) {
-
-        destination = Math.abs(start.x - finish.x) + Math.abs(start.y - finish.y);
-
-    }
-
 
 
 }
