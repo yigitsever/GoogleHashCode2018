7941a51c1a6e32a908533017d13afa95bc23dddf
Megalodone <coolnavarin@gmail.com>
2018-03-01 22:34:37 +0300

    City & Parser link

--- src/City.java
+++ src/City.java
@@ -3,13 +3,14 @@ import java.util.*;
 public class City {
 
     public static void main(String[] args) {
+        Parser p = new Parser();
+        Queue<Ride> rides = p.getPassengers();
+        ArrayList<Car> EmptyCars = p.getCars();
+        ArrayList<Car> FullCars = new ArrayList<>();
 
-        Queue<Ride> rides = new LinkedList<Ride>();
-        ArrayList <Car> Vehicle=new ArrayList<>();
-        
+        int T = p.getT();
+        int Bonus = p.getB();
 
 
-
-
-
+    }
 }
