eb72ca9bf57566a2692f9d7cf4efa8738f9d3273
cihadtekinbas <Matematik97@>
2018-03-01 21:26:19 +0200

    filled ride

--- src/Ride.java
+++ src/Ride.java
@@ -6,7 +6,7 @@ public class Ride {
     public Coord start;
     public Coord dest;
 
-    public void Ride(Coord start, Coord dest, int T_start, int T_latest) {
+    public  Ride(Coord start, Coord dest, int T_start, int T_latest) {
         this.Time_start = T_start;
         this.Time_latest = T_latest;
         this.start=start;
